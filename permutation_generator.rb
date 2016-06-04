e4905e9dadfcc423fa9093209c68f7fb72bc25af
Samuel Frade <SamuelSFrade@gmail.com>
2016-06-04 16:06:28 -0400

    Add permutation_generator.rb

---
+++ permutation_generator.rb
@@ -0,0 +1,29 @@
+def generate_permutation(target)
+  permutations = []
+  permutation_helper(target, [], permutations)
+  permutations
+end
+
+def permutation_helper(target, current, permutations)
+  leaf = true
+  target.each do |x|
+    unless current.include? x
+      leaf = false
+      cloned = make_copy(current)
+      cloned << x
+      permutation_helper(target, cloned, permutations)
+    end
+  end
+  if leaf
+    permutations << current
+  end
+end
+
+def make_copy(ary)
+  new_ary = []
+  ary.each do |x|
+    new_ary << x
+  end
+  new_ary
+end
+
